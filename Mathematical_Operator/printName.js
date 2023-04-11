b2c5315c62350994323db6853abd82eb77c6dcb3
JahirPendhari <jahirpp1999@gmail.com>
2023-04-11 08:38:21 +0530

    Day 1

---
+++ Mathematical_Operator/printName.js
@@ -0,0 +1,6 @@
+let name = "Tony Stark";
+console.log(name);
+name = "Jon Stark";
+console.log(name);
+name ="Merry Stark";
+console.log(name);
---
+++ Mathematical_Operator/tellDataTypes.js
@@ -0,0 +1,4 @@
+let nam = "Jahir Pendhari";
+let age = 25;
+console.log("Name-"+nam, "Age-"+age);
+console.log("name:-",typeof(nam),",age:-",typeof(age))
