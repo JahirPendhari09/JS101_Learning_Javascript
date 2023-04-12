eb1ab040efe951e2d4dfaa72125dfecdb93c701f
JahirPendhari <jahirpp1999@gmail.com>
2023-04-12 10:38:13 +0530

    Traffic Signal

---
+++ Mathematical_Operator/trafficSignal.js
@@ -0,0 +1,18 @@
+let traffic_light = "Green";
+
+if(traffic_light == "red")
+{
+  console.log("Stop");
+}
+else if(traffic_light == "Yellow")
+{
+  console.log("Be Ready for going");
+}
+else if(traffic_light == "Green")
+{
+  console.log(" Let's Go ");
+}
+else
+{
+  console.log("Traffic Signal is Not Working");
+}
