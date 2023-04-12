74ecff7ca4f2fddce70d0da9257c783e22c9547b
JahirPendhari <jahirpp1999@gmail.com>
2023-04-12 19:54:44 +0530

    Print Age

---
+++ Mathematical_Operator/calAge.js
@@ -0,0 +1,12 @@
+let year_of_birth = 1929;
+let age = 2023-year_of_birth;
+
+if(age>=13 && age<=19){
+  console.log("Teenage");
+}
+else if(age >=20 && age<= 29){
+  console.log("Twenties");
+}
+else{
+  console.log("You are Above 30+");
+}
