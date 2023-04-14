85607e57c5da18d3628508c4fe8794801da76a1d
JahirPendhari <jahirpp1999@gmail.com>
2023-04-14 09:43:33 +0530

    Find Second Maximum in Array

---
+++ Arrays/find_2_max_Array.js
@@ -0,0 +1,15 @@
+let arr = [144,2,4,9,24,96,5];
+let N= arr.length;
+
+let max =-Infinity;
+let sec_max =0;
+for(let i=0; i<N; i++){
+    if(max < arr[i]){
+        sec_max = max;
+        max = arr[i];
+    }else if(sec_max<arr[i]){
+        sec_max= arr[i];
+    }
+}
+console.log("Second Maxi :",sec_max);
+console.log(" Maxi :",max);
