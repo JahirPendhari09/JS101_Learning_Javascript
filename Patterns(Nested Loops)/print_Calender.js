65476f67697da43997bced55dcd6022f302eae56
JahirPendhari <jahirpp1999@gmail.com>
2023-04-18 15:29:41 +0530

    Printing Calendar Day and Months

---
+++ Patterns(Nested Loops)/print_Calender.js
@@ -0,0 +1,17 @@
+// Print Calender Dates and Respective Months
+
+for(let i=1; i<=12; i++){
+
+    if(i==2){
+      d =28;
+    }else if(i==4 || i==6 || i==9 ||i==11){
+      d =30;
+    }
+    else{
+      d =31;
+    }
+    for(j=1; j<=d; j++){
+      console.log(j,"-",i);
+    }
+    console.log("Month Completed",i);
+  }
