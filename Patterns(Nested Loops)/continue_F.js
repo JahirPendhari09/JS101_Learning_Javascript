3990ec10ace0c11b937151d715b7044a9ae94a1f
JahirPendhari <jahirpp1999@gmail.com>
2023-04-19 09:51:10 +0530

    Continue Statement

---
+++ Patterns(Nested Loops)/continue_F.js
@@ -0,0 +1,15 @@
+for(let i=1; i<=4; i++){
+    for(let j=1; j<=3; j++){
+      // if(j!=2){
+      //   console.log(i+"-"+j);
+      // }
+  
+      //or
+  
+      if(i==2){
+        continue;
+      }
+      console.log(i+"-"+j);
+      
+    }
+  }
