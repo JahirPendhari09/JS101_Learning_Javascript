a68ec1615b5ba1700ed415b24afe8a1835294d7a
JahirPendhari <jahirpp1999@gmail.com>
2023-04-27 10:08:03 +0530

    Join Function

--- Inbuilt/join_fun.js
+++ Inbuilt/join_fun.js
@@ -2,20 +2,9 @@
 
 function join(arr,key){
   let str ="";
-  for(let i=0; i<arr.length; i++){
-    if(i==arr.length-1){
-      str +=arr[i];
-    }else{
-      str +=arr[i]+key;
-    }
-  }
-  return str;
-}
 
-
-function join1(arr){
-  let str ="";
-  for(let i=0; i<arr.length; i++){
+  if(key ==undefined){
+    for(let i=0; i<arr.length; i++){
     if(i==arr.length-1){
       str +=arr[i];
     }else{
@@ -23,13 +12,19 @@ function join1(arr){
     }
   }
   return str;
+  }else{
+    for(let i=0; i<arr.length; i++){
+        if(i==arr.length-1){
+            str +=arr[i];
+        }else{
+            str +=arr[i]+key;
+         }
+    }
+  return str;
+  }
+
 }
-
 let arr =["jahir","shakil", "goku","nami","riya"];
-// only for passing array
-//let r = join1(arr);
-//console.log(r);
 
-// here we passing array and key .  eg.%,*,# 
 let k= join(arr,"-");
 console.log(k);
